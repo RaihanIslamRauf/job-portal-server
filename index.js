1b218a4574234601f36839e77dac0337acc3f06a
RaihanIslamRauf <md.raufislam27@gmail.com>
2025-02-03 01:50:09 +0600

    set up  jwt

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 const app = express();
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3yfc6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,6 +39,17 @@ async function run() {
     const jobApplicationCollection = client
       .db("jobPortal")
       .collection("job-applications");
+
+    // Auth related apis
+    app.post('/jwt', async(req,res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.JWT_SECRET , { expiresIn: '1h'});
+        res.cookie('token', token, {
+           httpOnly: true,
+           secure: false, //http://localhost:5173/signIn
+        })
+        .send({success: true});
+    })
 
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
